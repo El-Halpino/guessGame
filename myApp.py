d1b59220cc4fb0137aae74e8a2cf882dc3e6a5fe
Alan Halpin <C00229361@itcarlow.iegit config --global user.email C00229361@itcarlow.iegit config --global user.email C00229361@itcarlow.ie>
2019-11-15 20:09:48 +0200

    All Done

--- myApp.py
+++ myApp.py
@@ -5,9 +5,11 @@ import pickle
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def def_screen():
     return "Enter /guessform to start game.\nEnter /highScore to view the leaberboards"
+
 
 @app.route("/guessform")
 def start_guessForm():
@@ -28,40 +30,46 @@ def do_logic():
         numOfGuesses1 = int(session["numOfGuesses"])
         numOfGuesses1 = numOfGuesses1 + 1
         session["numOfGuesses"] = str(numOfGuesses1)
-        return render_template("guessform.html", the_title="Too High!", currentStatus="Too High!")
+        return render_template(
+            "guessform.html", the_title="Too High!", currentStatus="Too High!"
+        )
     elif guess < session["number"]:
         numOfGuesses1 = int(session["numOfGuesses"])
         numOfGuesses1 = numOfGuesses1 + 1
         session["numOfGuesses"] = str(numOfGuesses1)
-        return render_template("guessform.html", the_title="Too Low!", currentStatus="Too Low!")
+        return render_template(
+            "guessform.html", the_title="Too Low!", currentStatus="Too Low!"
+        )
+
 
 @app.route("/highScore")
 def display_highScore():
     with open("records.pickle", "rb") as pf:
-        data = sorted(pickle.load(pf))
+        data = sorted(pickle.load(pf), reverse = True)
     return render_template("highScore.html", the_title="HighScores!", myHighScores=data)
+
 
 @app.route("/setHighScore", methods=["POST", "GET"])
 def record_highscore():
     name = request.form["userName"]
     time = round(session["end"] - session["start"], 2)
-    userRecord = [name, session["numOfGuesses"], time]
+    time = str(time)
+    userRecord = [time, session["numOfGuesses"], name]
     with open("records.pickle", "rb") as pf:
         try:
             highScores = list(pickle.load(pf))
         except:
-            highScores = list()      
-    highScores.append(userRecord) 
+            highScores = list()
+    highScores.append(userRecord)
     finalHighScores = sorted(highScores)
-    rank = (
-    sorted(finalHighScores).index([name, session["numOfGuesses"], time]) + 1
-    )
+    rank = (finalHighScores.index([time, session["numOfGuesses"], name]) + 1)
     with open("records.pickle", "wb") as pf:
         pickle.dump(highScores, pf)
-        return render_template("/gameComplete.html", the_title="Guessing Game", position=rank)
+        return render_template(
+            "gameComplete.html", the_title="Guessing Game", position=rank
+        )
 
 
-   
 app.secret_key = "wrrrrrfegko3245tk50ekdgrge3t2ewdsf"
 
 if __name__ == "__main__":
